12b8a87533de376569ac5c25c12b33df186ad108
Thomas Walker <32672682+walker-tom@users.noreply.github.com>
2018-01-21 18:52:18 +0000

    more titles added

--- src/main/java/com/quantech/misc/Title.java
+++ src/main/java/com/quantech/misc/Title.java
@@ -1,5 +1,5 @@
 package com.quantech.misc;
 
 public enum Title {
-    Mr, Mrs, Miss, Ms, Dr, Professor, Sir, Master, Lord, Lady, Dame, Prof;
+    Mr, Mrs, Miss, Ms, Mx, Dr, Professor, Sir, Master, Lord, Lady, Dame, Prof, mr, mrs, ms, dr, professor, sir, master, lord, lady, dame, prof, mx;
 }
