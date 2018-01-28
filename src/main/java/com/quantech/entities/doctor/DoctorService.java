2b306224bd0e5a109d30c869ab424e4dd84184ac
nuhat <nt6888@bristol.ac.uk>
2018-01-28 11:04:27 +0000

    started working on doctor service methods, beginning with sorting

--- src/main/java/com/quantech/entities/doctor/DoctorService.java
+++ src/main/java/com/quantech/entities/doctor/DoctorService.java
@@ -6,7 +6,9 @@ import com.quantech.entities.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.print.Doc;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -24,6 +26,74 @@ public class DoctorService {
         List<Doctor> doctors = new ArrayList<>();
         doctorRepository.findAll().forEach(doctors::add);
         return doctors;
+    }
+
+
+    // A comparator used to sort doctors by their date renewed, most recent first.
+    private Comparator<Doctor> dateRenewedMostRecentFirstComparator() {
+       return (o1,o2) -> {
+           if(o1.getFirstName() != null && o2.getFirstName() != null){
+               return (o1.getLastRenewed().compareTo(o2.getLastRenewed()));
+           }
+           return 0;
+       };
+    }
+
+    // A comparator used to sort doctors by their date renewed, most recent last.
+    private Comparator<Doctor> dateRenewedMostRecentLastComparator() {
+        return (o1,o2) -> {
+            if(o1.getFirstName() != null && o2.getFirstName() != null){
+                return -(o1.getLastRenewed().compareTo(o2.getLastRenewed()));
+            }
+            return 0;
+        };
+    }
+
+    /**
+     * Sort the given list of doctors by their first name, alphabetically.
+     * @param list The list of doctors.
+     * @return A sorted list of doctors, by first name.
+     */
+    public List<Doctor> sortPatientsByFirstName(List<Doctor> list) {
+        return sortList(list,firstNameAlphabeticalComparator());
+    }
+
+    // A comparator that can be used to sort the doctors by their first name, alphabetically.
+    private Comparator<Doctor> firstNameAlphabeticalComparator() {
+        return (o1, o2) -> {
+            if(o1.getFirstName() != null && o2.getFirstName() != null){
+                if (o1.getFirstName().compareTo(o2.getFirstName()) == 0)
+                    return lastNameAlphabeticalComparator().compare(o1,o2);
+                else
+                    return o1.getFirstName().compareTo(o2.getFirstName());
+            }
+            return 0;
+        };
+    }
+
+    /**
+     * Sort the given list of doctors by their last name, alphabetically.
+     * @param list The list of doctors.
+     * @return A sorted list of doctors, by last name.
+     */
+    public List<Doctor> sortDoctorsByLastName(List<Doctor> list) {
+        return sortList(list,lastNameAlphabeticalComparator());
+    }
+
+    // A comparator that can be used to sort the doctors by their first name, alphabetically.
+    private Comparator<Doctor> lastNameAlphabeticalComparator() {
+        return (o1, o2) -> {
+            if(o1.getLastName() != null && o2.getLastName() != null){
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+            return 0;
+        };
+    }
+
+    // Returns a list sorted using the input comparator.
+    private List<Doctor> sortList(List<Doctor> l, Comparator<Doctor> c) {
+        l.sort(c);
+        return l;
     }
 
     /**
